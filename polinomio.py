84917452f6e28580a8b3787564ce65096c577bca
spfernandes91 <42682348+spfernandes91@users.noreply.github.com>
2026-04-13 16:48:18 -0300

    Create polinomio.py

---
+++ polinomio.py
@@ -0,0 +1,41 @@
+#Definição: Este programa recebe o grau e os coeficientes e calcula o polinomio de um valor pré determinado
+#Autor: Sérgio Fernandes
+#Data: 13/10/26
+#Versão:0.0.1
+'''
+a.append(5)
+a.append(6)
+print(*a)
+'''
+
+#Alocação de memória
+n= 0
+i=0 
+a = [] 
+aux=0
+x=2 
+result=0
+
+
+#Entrada de dados
+n=int(input("Digite o grau do polinômio:"))
+while(i<=n ):
+    aux=int(input("Digite os n coeficientes:"))
+    a.append(aux)
+    i=i +1
+ 
+#rint(* a)
+
+
+#Procesamento de dados
+i=0 
+while(i<n):
+    result =+a[i]* (x **i)
+    #rint(f'{a[i]}')
+    i=i+1
+    print(f'{result}')
+                        
+    
+    
+    
+
